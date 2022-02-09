4fdbaa207cade8ebad1ee55b1327cd8e50247867
keeko <787018661@qq.com>
2022-02-09 21:07:36 +0800

    [demo] umi2项目动态路由，路由参数获取

---
+++ src/pages/class/$id.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+export default class Layout extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>id: {this.props.match.params.id}</h1>
+      </div>
+    );
+  }
+}
