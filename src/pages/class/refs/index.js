7a994ced6812b34b3f3cb64aa8cbfda2833dd344
keeko <787018661@qq.com>
2022-02-15 23:37:29 +0800

    [demo] ref操作子组件

--- src/pages/class/refs/index.js
+++ src/pages/class/refs/index.js
@@ -1,4 +1,5 @@
 import React, { Component, createRef } from 'react';
+import Child from './child';
 
 export default class Refs extends Component {
 
@@ -8,6 +9,7 @@ export default class Refs extends Component {
 
     };
     this.domRef = createRef();
+    this.childRef = createRef();
   }
 
   componentDidMount() {
@@ -15,8 +17,22 @@ export default class Refs extends Component {
     // ↑ 打印出来的结果this.domRef.current 就是h1节点
     console.log("innerHTML->", this.domRef.current.innerHTML);
 
+    console.log("childRef->", this.childRef.current); // 组件实例
+
     // 如果没有在标签上写ref属性，在componentDidMount里打印结果 { current: null }
     // 疑问： createRef在什么时候执行？
+  }
+
+  // 可以使用A1+A2的写法，注意A2必须用 { () => this.handleChild("str") }
+  // ↓ A1不能使用这种写法
+  // handleChild = (str) => {
+  //   console.log("this->", this);
+  //   this.childRef.current.changeText(str);
+  // }
+
+  // ↓ B1
+  handleChild(str) {
+    this.childRef.current.changeText(str);
   }
 
   render() {
@@ -24,6 +40,13 @@ export default class Refs extends Component {
       <div>
         <h1 ref={this.domRef}>refs text</h1>
         {/* <h1>refs text</h1> */}
+
+        <Child ref={this.childRef}></Child>
+        {/* ↓ A2不能使用这种写法 */}
+        {/* <button onClick={ () => this.handleChild("new") }>修改child组件内的值</button> */}
+
+        {/* ↓ B2 */}
+        <button onClick={() => this.handleChild("new") }>修改child组件内的值</button>
       </div>
     )
   }
