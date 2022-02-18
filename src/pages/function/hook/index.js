03a47d7decd3a760cdf412a9f2cd648c391272d4
keeko <787018661@qq.com>
2022-02-18 20:31:19 +0800

    [demo] react hook useMemo及useCallback

--- src/pages/function/hook/index.js
+++ src/pages/function/hook/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useMemo, useCallback } from 'react';
 // ↑ 在function组件中使用useState方法设置变量和更改变量
 
 export default function(props) {
@@ -6,6 +6,7 @@ export default function(props) {
   // useState(argument) argument是非必填项，如果不填。默认state值为undefined
 
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("test demo");
 
   // useEffect(func, array), array是非必选项
   // ↓ 类似于componentDidMount, componentWillUnmount生命周期
@@ -110,13 +111,36 @@ export default function(props) {
   // 1、一般在项目中在useEffect中做异步操作
   // 2、在useLayoutEffect中操作dom对象或改变浏览器的显示效果
 
+  /* 
   const handleCount = () => {
     setCount(count + 1);
     // setCount(count++); // ← 不能这样写，报错"count" is read-only
-  };
+  }; 
+  */
+
+  const noCacheText = () => {
+    console.log("text changed"); 
+    return text;
+  }
+
+  const memoText = useMemo(() => {
+    console.log("useMemo text changed");
+    return text;
+  }, [text])
+  // ↑ 如果依赖项text没发生改变，视图发生改变，memoText不会改变。
+  // useMemo用来做性能的优化，返回的是一个经过缓存的值
+
+  const handleCount = useCallback(() => {
+    console.log("count changed");;
+    setCount(count + 1);
+  }, [count])
+
   return (
     <div>
       <h1 onClick={handleCount}>count: {count}</h1>
+      {/* <h1>text: {noCacheText()}</h1> */}
+      <h1>text: {memoText}</h1>
+
     </div>
   );
 }
