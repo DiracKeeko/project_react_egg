d01f978ba976f6c6d7618baf40ae78b9518b1fb7
keeko <787018661@qq.com>
2022-03-25 00:22:23 +0800

    [feat] home header init

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,6 +3,8 @@ import Header from './components/header';
 import Search from './components/search';
 import Hot from './components/hot';
 
+import "./index.less";
+
 export default function (props) {
   const [state, setState] = useState();
 
