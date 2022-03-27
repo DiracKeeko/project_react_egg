b024abffb57ede35a4d5df139bbba0937f86f719
keeko <787018661@qq.com>
2022-03-27 11:18:56 +0800

    [chore] 搜索界面初始化,首页跳转搜索界面

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  publicPath: "./",
   nodeModulesTransform: {
     type: 'none',
   },
@@ -23,6 +24,11 @@ export default defineConfig({
           path: '/user',
           component: './user/index',
           title: '我的'
+        },
+        {
+          path: '/search',
+          component: './search/index',
+          title: '搜索'
         },
       ]
     }
--- src/pages/home/components/search/index.js
+++ src/pages/home/components/search/index.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Picker, List, Calendar, Button } from 'antd-mobile';
+import { Picker, List, Calendar, Button, Toast } from 'antd-mobile';
 import dayjs from 'dayjs';
+import { history } from 'umi';
 
 export default function (props) {
-  const [citys, setCitys] = useState([
-    [
-      { label: '杭州', value: '10001' },
-      { label: '苏州', value: '10002' },
-    ],
-  ]);
+  // const [citys, setCitys] = useState([
+  //   [
+  //     { label: '杭州', value: '10001' },
+  //     { label: '苏州', value: '10002' },
+  //   ],
+  // ]);
 
   const [selectedCity, setSelectedCity] = useState(['10001']);
 
@@ -28,6 +29,21 @@ export default function (props) {
     const endTimeStr = dayjs(endTime).format('YYYY-MM-DD');
     setTimes(startTimeStr + '~' + endTimeStr);
     setDateShow(false);
+  };
+
+  const handleClick = () => {
+    if (times.includes('~')) {
+      history.push({
+        pathname: '/search',
+        query: {
+          code: selectedCity,
+          startTime: times.split('~')[0],
+          endTime: times.split('~')[1],
+        },
+      });
+    } else {
+      Toast.fail('请选择时间');
+    }
   };
 
   useEffect(() => {}, []);
@@ -55,7 +71,7 @@ export default function (props) {
         <p className="search-time_right">{times}</p>
       </div>
       {/* 点击按钮 */}
-      <Button type="warning" size="large">
+      <Button type="warning" size="large" onClick={handleClick}>
         搜索民宿
       </Button>
 
