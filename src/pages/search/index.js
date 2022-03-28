9b8336650921b5a7a8e5322260dc89585f96b68e
keeko <787018661@qq.com>
2022-03-28 16:29:45 +0800

    [chore] 搜索界面01

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,11 +1,71 @@
 import React, { useState, useEffect } from 'react';
+import { SearchBar, ActivityIndicator } from 'antd-mobile';
+import { useHttpHook, useObserverHook, useImgHook } from '@/hooks';
 
 import './index.less';
 
 export default function (props) {
-  const [state, setState] = useState();
+  const [houseName, setHouseName] = useState('');
+
+  const [houses, loading] = useHttpHook({
+    url: '/house/search',
+    body: {
+      pageNum: 1
+    }
+  });
+
+  const handleChange = (value) => {
+    // console.log(value)
+    setHouseName(value);
+  };
+
+  const handleCancle = () => {
+    _handleSubmit('');
+  };
+
+  const handleSubmit = (value) => {
+    // console.log(value)
+    _handleSubmit(value);
+  };
+
+  const _handleSubmit = (value) => {
+    // setHouseName(value);
+    // setHouseSubmitName(value);
+    // setPage(CommonEnum.PAGE);
+    // setHouseLists([]);
+  };
 
   useEffect(() => {}, []);
 
-  return <div>search</div>;
+  return (
+    <div className="search-page">
+      {/**顶部搜索栏 */}
+      <SearchBar
+        placeholder="搜索民宿"
+        value={houseName}
+        onChange={handleChange}
+        onCancel={handleCancle}
+        onSubmit={handleSubmit}
+      />
+      {/**搜索结果 */}
+      {loading ? (
+        <ActivityIndicator toast />
+      ) : (
+        <div className="result">
+          {houses.map((item) => (
+            <div className="item" key={item.id}>
+              <img
+                alt="img"
+                src={item.img}
+              />
+              <div className="item-right">
+                <div className="title">{item.title}</div>
+                <div className="price">{item.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
