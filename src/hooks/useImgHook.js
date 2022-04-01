a4bb210bf159b0429cac70cc91d5486f6db4d0cd
Keeko <787018661@qq.com>
2022-04-01 19:45:51 +0800

    [feat] add useImgHook

---
+++ src/hooks/useImgHook.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+
+/**
+ * 1，监听图片是否进入可视区域；
+ * 2，将src属性的值替换为真实的图片地址，占位图片地址为data-src
+ * 3，停止监听当前的节点
+ * @param {*} ele 
+ * @param {*} callback 
+ * @param {*} watch 
+ */
+
+let sectionObserver;
+export default function (ele, callback, watch = []) {
+  useEffect(() => {
+    const nodes = document.querySelectorAll(ele);
+    if (nodes && nodes.length) {
+      sectionObserver = new IntersectionObserver(entries => {
+        callback && callback(entries);
+        entries.forEach(item => {
+          // console.log(item);
+          if (item.isIntersecting) {
+            const dataSrc = item.target.getAttribute('data-src');
+            item.target.setAttribute("src", dataSrc);
+            sectionObserver.unobserve(item.target);
+          }
+        })
+      });
+      nodes.forEach(item => {
+        sectionObserver.observe(item);
+      })
+    }
+    return () => {
+      if (nodes && nodes.length && sectionObserver) {
+        sectionObserver.disconnect();
+      }
+    }
+  }, watch);
+}
