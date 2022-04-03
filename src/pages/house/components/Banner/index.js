db5c43637258813c4effb0623f6ceac2b4cb6034
keeko <787018661@qq.com>
2022-04-03 22:22:37 +0800

    [feat] 引入滑动组件react-awesome-swiper

--- src/pages/house/components/Banner/index.js
+++ src/pages/house/components/Banner/index.js
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from 'react';
+import AwesomeSwiper from "react-awesome-swiper";
 
 export default function(props){
-  const [state, setState] = useState()
+  const [config, setConfig] = useState({
+    loop: true,
+    autoplay: {
+      delay: 1500
+    },
+    pagination: {
+      el: ".swiper-pagination", //dom节点
+    }
+  })
 
   useEffect(() => {
 
   }, [])
 
   return (
-    <div>
-      banner
-    </div>
+    <AwesomeSwiper className="banner" config={config}>
+      <div className="swiper-wrapper">
+        <div className="swiper-slide">
+          <img alt="banner" src={""}></img>
+        </div>
+        <div className="swiper-slide">
+          <img alt="banner" src={""}></img>
+        </div>
+        <div className="swiper-slide">
+          <img alt="banner" src={""}></img>
+        </div>
+      </div>
+      <div className="swiper-pagination"></div>
+    </AwesomeSwiper>
   )
 }
