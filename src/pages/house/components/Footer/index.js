41f8ac8048097ea0a9944fec2b49010c87c7b0e5
Keeko <787018661@qq.com>
2022-04-06 16:02:58 +0800

    [feat] house页面footer

--- src/pages/house/components/Footer/index.js
+++ src/pages/house/components/Footer/index.js
@@ -1,15 +1,45 @@
 import React, { useState, useEffect } from 'react';
+import { Modal } from '@/components';
+import { TextareaItem, Button } from 'antd-mobile';
 
-export default function(props){
-  const [state, setState] = useState()
+export default function (props) {
+  const [show, setShow] = useState(false);
 
-  useEffect(() => {
+  const handleClick = () => {
+    setShow(true);
+  };
 
-  }, [])
+  const handleClose = () => {
+    setShow(false)
+  };
+
+  const handleChange = (value) => {
+    console.log(value);
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <div>
-      footer
+      <div className="footer" onClick={handleClick}>
+        评论-
+      </div>
+      <Modal
+        show={show}
+        styleBody={{
+          height: '220px',
+          bottom: '0px',
+          top: 'unset',
+        }}
+        onClose={handleClose}
+      >
+        <div className="modal-comment">
+          <TextareaItem rows={2} count={200} onChange={handleChange} />
+          <Button className="comment-btn" type="warning">
+            评论
+          </Button>
+        </div>
+      </Modal>
     </div>
-  )
+  );
 }
--- src/pages/house/components/Lists/index.js
+++ src/pages/house/components/Lists/index.js
@@ -8,8 +8,22 @@ export default function(props){
   }, [])
 
   return (
-    <div>
-      lists
+    <div className="commeent">
+      <h1 className="comment-title">评论</h1>
+      <div className="comment-lists">
+        <div className="comment-lists_item">
+          <img alt="user" className="avatar" src={"http://img3.mukewang.com/szimg/5d1032ab08719e0906000338-360-202.jpg"}/>
+          <div className="right">
+            <div className="right-top">
+              <p>{"user"}</p>
+              <p>{"time"}</p>
+            </div>
+            <div className="right-bottom">
+              {"info"}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
