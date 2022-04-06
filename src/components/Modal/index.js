f6abef791fdda4faf5aa955a85b5350c8e480b9a
Keeko <787018661@qq.com>
2022-04-06 16:02:14 +0800

    [feat] Modal组件修改

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
-import CreatePortal from "@/components/CreatePortal";
-import { Icon } from "antd-mobile";
+import CreatePortal from '../CreatePortal';
+import { Icon } from 'antd-mobile'; 
 
 const Styles = {
   modal: {
-      position: "relative",
-      top: '0',
-      left: '0',
-      zIndex: '999'
-    },
-    body: { 
-      backgroundColor: '#fff',
-      position: 'fixed',
-      height: '100%',
-      width: '100%',
-      top: '0',
-      left: '0',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    close: {
-      position: "fixed",
-      top: "10px",
-      right: "10px"
-    }
+    position: 'relative',
+    top: '0',
+    left: '0',
+    zIndex: '999'
+  },
+  body: {
+    backgroundColor: '#fff',
+    position: 'fixed',
+    height: '100%',
+    width: '100%',
+    top: '0',
+    left: '0',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  close: {
+    position: 'absolute',
+    top: '10px',
+    right: '10px'
+  }
 };
 
 export default class Modal extends Component {
@@ -32,27 +32,41 @@ export default class Modal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      showModal: false
     };
   }
 
-  handleClose = () => {
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      showModal: nextProps.show
+    });
+  }
+
+  handleClose = ()=>{
     const { onClose } = this.props;
     onClose && onClose();
   }
 
   render() {
-    const { show } = this.props;
-    console.log("show->", show);
+    const { styleBody, styleClose } = this.props;
+    const { showModal } = this.state;
+    const _styleBody = {
+      ...Styles.body,
+      ...styleBody
+    };
+    const _styleClose = {
+      ...Styles.close,
+      ...styleClose
+    };
     return (
-      <div>
-        { show ? <CreatePortal style={ Styles.modal }>
-          <div style={Styles.body}>
-            { this.props.children }
-            <Icon type="cross" size="lg" style={ Styles.close } onClick={this.handleClose}></Icon>
+      <>
+        {showModal ? <CreatePortal style={Styles.modal}>
+          <div style={_styleBody}>
+            {this.props.children}
+            <Icon type='cross' size='lg' style={_styleClose} onClick={this.handleClose} />
           </div>
-        </CreatePortal> : null }
-      </div>
+        </CreatePortal> : null}
+      </>
     )
   }
 }
