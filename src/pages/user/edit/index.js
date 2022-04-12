e79006f04e38a663bfcb8201fbe2a9f7278d2a93
Keeko <787018661@qq.com>
2022-04-12 17:18:39 +0800

    [feat] user页面引入rc-form处理表单

--- src/pages/user/edit/index.js
+++ src/pages/user/edit/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { List, ImagePicker, Toast, InputItem, Button } from 'antd-mobile';
+import { createForm } from 'rc-form';
 
-export default function (props) {
+function Edit(props) {
   const [files, setFiles] = useState([]);
-
+  const { getFieldProps, validateFields } = props.form;
   const handleChange = (files) => {
     // files是一个数组，数组内容是选中的file信息
     // [{...}, {...}]
@@ -15,8 +16,23 @@ export default function (props) {
     setFiles(files);
   };
 
-  const handleSubmit = () => {};
-  useEffect(() => {}, []);
+  const handleSubmit = () => {
+    if (!files.length) {
+      Toast.fail("请上传图片");
+      return;
+    }
+    validateFields((err, val) => {
+      if (err) {
+        Toast.fail("请完整填写信息");
+        return;
+      } else {
+
+      }
+    })
+  };
+  useEffect(() => {
+    // console.log('props->', props);
+  }, []);
 
   return (
     <div className="user-edit">
@@ -29,10 +45,26 @@ export default function (props) {
           />
         </List.Item>
         <List.Item>
-          <InputItem placeholder="电话">电话：</InputItem>
+          <InputItem
+            placeholder="电话"
+            {...getFieldProps('tel', {
+              rules: [{ required: true }],
+              initialValue: '0571-000',
+            })}
+          >
+            电话：
+          </InputItem>
         </List.Item>
         <List.Item>
-          <InputItem placeholder="签名">签名：</InputItem>
+          <InputItem
+            placeholder="签名"
+            {...getFieldProps('sign', {
+              rules: [{ required: true }],
+              initialValue: 'sign',
+            })}
+          >
+            签名：
+          </InputItem>
         </List.Item>
       </List>
       <Button
@@ -45,3 +77,8 @@ export default function (props) {
     </div>
   );
 }
+
+// createForm(options)(React component)
+// createForm方法会给React component组件的props绑定一些属性
+// options不传参，就添加默认配置项
+export default createForm()(Edit);
