8013863231c00cddcfbedc2c36a9ec31926d74f2
Keeko <787018661@qq.com>
2022-04-25 10:50:38 +0800

    [feat] 优化前端用户登录插件,判断未登录,跳转login

--- server/lib/plugin/egg-auth/app/middleware/auth.js
+++ server/lib/plugin/egg-auth/app/middleware/auth.js
@@ -4,12 +4,12 @@ module.exports = (options) => {
   return async (ctx, next) => {
     const url = ctx.request.url;
     console.log("url", url);
-    const user = ctx.session.user;
+    const user = ctx.session[ctx.username];
 
     if (!user && !options.exclude.includes(ctx.request.url.split("?")[0])) {
       ctx.body = {
-        code: "1",
-        msg: "用户未登录",
+        status: 1001,
+        errMsg: "用户未登录",
       };
     } else {
       await next();
