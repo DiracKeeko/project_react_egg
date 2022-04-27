b620452be63d72dfc22ff7d077ebc38f3b2669eb
keeko <787018661@qq.com>
2022-04-27 23:58:16 +0800

    [feat] 增加citys mock数据

--- server/app/controller/commons.js
+++ server/app/controller/commons.js
@@ -12,19 +12,21 @@ class CommonsController extends BaseController {
         }
       );
       // console.log(result)
-      // if (result.status === 200) {
-      //   const cityArr = [
-      //     { label: '杭州', value: '10001' },
-      //     { label: '苏州', value: '10002' },
-      //     { label: '上海', value: '10003' },
-      //     { label: '绍兴', value: '10004' },
-      //     { label: '嘉兴', value: '10005' },
-      //   ]
-      //   this.success(cityArr);
-      // }
-      if (result.status === 200 && result.data.citys) {
-        this.success(result.data.citys);
-      } 
+      if (result.status === 200) {
+        const cityArr = [
+          [
+            { label: '杭州', value: '10001' },
+            { label: '苏州', value: '10002' },
+            { label: '上海', value: '10003' },
+            { label: '绍兴', value: '10004' },
+            { label: '嘉兴', value: '10005' }
+          ]
+        ]
+        this.success(cityArr);
+      }
+      // if (result.status === 200 && result.data.citys) {
+      //   this.success(result.data.citys);
+      // } 
       else {
         this.error("获取城市数据失败");
       }
